039668c075b3517ef5180544b21e3d20804da27c
leonardo-marinho <leonardo.msnt@outlook.com>
2023-09-08 12:19:05 -0300

    Moved FE folders to client folder

---
+++ src/client/utils/moneyToText.spec.ts
@@ -0,0 +1,17 @@
+import { moneyToText } from './moneyToText';
+
+describe('moneyToText function', () => {
+  it('should render with symbol', () => {
+    expect(moneyToText(10.12345, '$')).toEqual('$10.12');
+  });
+
+  describe('fixed parameter', () => {
+    it('should fix value by 2 by default', () => {
+      expect(moneyToText(10.12345, '$')).toEqual('$10.12');
+    });
+
+    it('should fix value', () => {
+      expect(moneyToText(10.12345, '$', 1)).toEqual('$10.1');
+    });
+  });
+});
